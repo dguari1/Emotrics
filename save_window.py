a0ccaf01c0604468a6025bb99a380955c0de2492
Diego L. Guarin <diego.guarinlopez@mail.mcgill.ca>
2017-12-04 14:16:50 -0500

    new way to save

--- save_window.py
+++ save_window.py
@@ -4,15 +4,11 @@ Created on Sat Dec  2 10:33:30 2017
 
 @author: Diego L.Guarin -- diego_guarin at meei.harvard.edu
 """
-
-import cv2
-import numpy as np
-import time
 import os
 from PyQt5 import QtWidgets
 from PyQt5 import QtGui
 from PyQt5 import QtCore
-from PyQt5.QtWidgets import QLabel, QLineEdit, QPushButton, QGridLayout, QFileDialog, QDialog, QComboBox
+from PyQt5.QtWidgets import QLabel, QLineEdit, QPushButton, QGridLayout, QFileDialog, QDialog, QComboBox, QGroupBox
 
 
 from utilities import get_info_from_txt
@@ -52,17 +48,25 @@ class SaveWindow(QDialog):
                                 #the file that the results will be saved to, 
                                 #if is saving a patient then this option is not 
                                 #avaliable
-        self._file_name = file_name
+                                
+        self._name_of_file = file_name  #this variable stores the name of the 
+                                        #file, it won't be modified during 
+                                        #execution                        
+                                
+        self._file_name = file_name     #this variable stores the name of the 
+                                        #file to be displayed. It will be 
+                                        #modified during execution 
+        filename, file_extension = os.path.splitext(self._file_name) 
         delimiter = os.path.sep
-        temp=self._file_name.split(delimiter)
+        temp=filename.split(delimiter)
         photo_location = temp[0:-1]
-        photo_name=temp[-1]
         photo_location = delimiter.join(photo_location)
-
+        photo_name=temp[-1]
+       
         
-        self._photo_name = photo_name[0:-4]  #path + file name
+        self._file_name = photo_name  #path + file name
         self._photo_location = photo_location
-        self._ID = '' #unique identifier
+        self._ID = photo_name #unique identifier
         self._prevspost = '' #pre-treatment vs post-treatment
         self._surgery = '' #type of surgery
         self._expression = '' #type of expression
@@ -86,24 +90,54 @@ class SaveWindow(QDialog):
         spacerv = QtWidgets.QWidget(self)
         spacerv.setFixedSize(0,10)
         
+        
+        
+        
+        
   
         file = QLabel('File Name:')
         self._file = QLineEdit(self)
-        self._file.setText(self._photo_name)
+        self._file.setText(self._file_name)
         
-        SelectFolderButton = QPushButton('Select &Folder', self)
-        SelectFolderButton.setFixedWidth(150)
-        SelectFolderButton.clicked.connect(self.SelectFolder)
+        self.SelectFolderButton = QPushButton('Select &Folder', self)
+        self.SelectFolderButton.setFixedWidth(150)
+        self.SelectFolderButton.clicked.connect(self.SelectFolder)
         self._SelectFolder = QLineEdit(self)
         self._SelectFolder.setText(self._photo_location) 
         self._SelectFolder.setFixedWidth(350)
         
+        
+        NewFileBox = QGroupBox('Create new File')
+        NewFileBoxLayout = QGridLayout()
+        NewFileBoxLayout.addWidget(file,0,0)
+        NewFileBoxLayout.addWidget(spacerh,0,1)
+        NewFileBoxLayout.addWidget(self._file,0,2)
+        NewFileBoxLayout.addWidget(spacerv,1,0)
+        NewFileBoxLayout.addWidget(self.SelectFolderButton,2,0)
+        NewFileBoxLayout.addWidget(self._SelectFolder,2,2)
+        NewFileBox.setLayout(NewFileBoxLayout)
+        
+        
+        SelectFileButton = QPushButton('&Select File', self)
+        SelectFileButton.setFixedWidth(150)
+        SelectFileButton.clicked.connect(self.SelectFile)       
+        self._SelectFile = QLineEdit(self)
+        self._SelectFile.setText(self._file_to_save) 
+        
+        
+        AppendFileBox = QGroupBox('Append to Existing File')
+        AppendFileBoxLayout = QGridLayout()
+        AppendFileBoxLayout.addWidget(SelectFileButton,0,0)
+        AppendFileBoxLayout.addWidget(spacerh,0,1)
+        AppendFileBoxLayout.addWidget(self._SelectFile,0,2)
+        AppendFileBox.setLayout(AppendFileBoxLayout)
+        
+        
         Identifier = QLabel('Photo Identifier:')
+        Identifier.setFixedWidth(120)
         self._Identifier = QLineEdit(self)
         self._Identifier.setText(self._ID)
         
-        
-        #get the second photo
         PrevsPost = QLabel('Pre or Post Procedure:')
         self._PrevsPost = QComboBox()
         self._PrevsPost.setFixedWidth(110)
@@ -124,12 +158,34 @@ class SaveWindow(QDialog):
         self._AddtitionalComments = QLineEdit(self)
         self._AddtitionalComments.setText(self._other)       
         
+        AdditionalInformationBox = QGroupBox('Optional Information')
+        AdditionalInformationBoxLayout = QGridLayout()
         
-        SelectFileButton = QPushButton('&Select File', self)
-        SelectFileButton.setFixedWidth(150)
-        SelectFileButton.clicked.connect(self.SelectFile)       
-        self._SelectFile = QLineEdit(self)
-        self._SelectFile.setText(self._file_to_save) 
+        AdditionalInformationBoxLayout.addWidget(Identifier,0,0)
+        AdditionalInformationBoxLayout.addWidget(spacerh,0,1)
+        AdditionalInformationBoxLayout.addWidget(self._Identifier,0,2)
+        
+        AdditionalInformationBoxLayout.addWidget(spacerv,1,0)
+        
+        AdditionalInformationBoxLayout.addWidget(PrevsPost,2,0)
+        AdditionalInformationBoxLayout.addWidget(self._PrevsPost,2,2)
+        
+        #AdditionalInformationBoxLayout.addWidget(spacerv,3,0) 
+        
+        AdditionalInformationBoxLayout.addWidget(SurgeryType,4,0)
+        AdditionalInformationBoxLayout.addWidget(self._SurgeryType,4,2)
+        
+        #AdditionalInformationBoxLayout.addWidget(spacerv,5,0) 
+
+        AdditionalInformationBoxLayout.addWidget(ExpressionType,6,0)
+        AdditionalInformationBoxLayout.addWidget(self._ExpressionType,6,2)
+        
+        #AdditionalInformationBoxLayout.addWidget(spacerv,7,0)         
+
+        AdditionalInformationBoxLayout.addWidget(AddtitionalComments,8,0)
+        AdditionalInformationBoxLayout.addWidget(self._AddtitionalComments,8,2)    
+        
+        AdditionalInformationBox.setLayout(AdditionalInformationBoxLayout)
         
         
         
@@ -142,56 +198,26 @@ class SaveWindow(QDialog):
         CancelButton.clicked.connect(self.Cancel)
         
         
+        ButtonBox = QGroupBox('')
+        ButtonBoxLayout = QGridLayout()
+        ButtonBoxLayout.addWidget(SaveButton,0,0,QtCore.Qt.AlignCenter)
+        ButtonBoxLayout.addWidget(spacerh,0,1)
+        ButtonBoxLayout.addWidget(CancelButton,0,2,QtCore.Qt.AlignCenter)
+        ButtonBox.setLayout(ButtonBoxLayout)
+        ButtonBox.setStyleSheet("QGroupBox {  border: 0px solid gray;}");
+        
         layout = QGridLayout()
-        layout.addWidget(file, 1,1,1,1)
-        layout.addWidget(spacerh,1,2)
-        layout.addWidget(self._file, 1,3,1,1)
-        
-        layout.addWidget(spacerv,2,0)
-        
-        layout.addWidget(SelectFolderButton, 3,1,1,1)
-        layout.addWidget(self._SelectFolder , 3, 3, 1, 1)
-       
-        layout.addWidget(spacerv,4,0)
-        
-        layout.addWidget(Identifier, 5,1,1,1)
-        layout.addWidget(self._Identifier , 5, 3, 1, 1)
-        
-        layout.addWidget(spacerv,6,0)
-        
-        layout.addWidget(PrevsPost, 7,1,1,1)
-        layout.addWidget(self._PrevsPost, 7,3,1,1)
-        
-        layout.addWidget(spacerv,8,0)
 
-        layout.addWidget(SurgeryType,9,1,1,1)
-        layout.addWidget(self._SurgeryType,9, 3, 1, 1)
+        layout.addWidget(NewFileBox,0,0,2,2)
         
-        layout.addWidget(spacerv,10,0)
+        layout.addWidget(spacerv,1,0)
         
-        layout.addWidget(ExpressionType, 11,1,1,1)
-        layout.addWidget(self._ExpressionType, 11,3,1,1)
+        layout.addWidget(AppendFileBox,2,0,1,2)
+               
         
-        layout.addWidget(spacerv,12,0)
+        layout.addWidget(AdditionalInformationBox,4,0,8,2)        
         
-        layout.addWidget(AddtitionalComments, 13,1,1,1)
-        layout.addWidget(self._AddtitionalComments, 13,3,1,1) 
-        
-        layout.addWidget(spacerv,14,0)
-        
-        layout.addWidget(SelectFileButton,15,1,1,1)
-        layout.addWidget(self._SelectFile,15,3,1,1)
-        
-        layout.addWidget(spacerv,16,0)
-        
-        layout.addWidget(SaveButton,17,1,1,1, QtCore.Qt.AlignCenter)
-        
-        layout.addWidget(CancelButton,17,3,1,1, QtCore.Qt.AlignCenter)
-
-        
-        
-        #self.main_Widget.setLayout(layout)
-        #self.setCentralWidget(self.main_Widget)
+        layout.addWidget(ButtonBox,17,0,1,2)
         
         self.setLayout(layout)
         
@@ -211,6 +237,18 @@ class SaveWindow(QDialog):
         if not name:
             pass
         else:
+            
+            if not self._SelectFolder.isEnabled():        
+                self._file.setEnabled(True)
+                self._SelectFolder.setEnabled(True)
+                self._SelectFile.setText('') 
+                
+                filename, file_extension = os.path.splitext(self._name_of_file) 
+                delimiter = os.path.sep
+                temp=filename.split(delimiter)
+                photo_name=temp[-1]
+                self._file.setText(photo_name)
+                
             name = os.path.normpath(name)
             self._photo_location = name
             self._SelectFolder.setText(self._photo_location) 
@@ -226,13 +264,24 @@ class SaveWindow(QDialog):
         else:
             name = os.path.normpath(name)
             delimiter = os.path.sep
-            temp=name.split(delimiter)
+            filename, file_extension = os.path.splitext(name)           
+            temp=filename.split(delimiter)
             photo_location = temp[0:-1]
             photo_location = delimiter.join(photo_location)
+            photo_name=temp[-1]
+            
+            self._file_name = photo_name
+            
+            self._file.setText(self._file_name)
+            self._file.setEnabled(False)
+            
+            
             
 
             self._photo_location = photo_location
             self._SelectFolder.setText(self._photo_location) 
+            self._SelectFolder.setEnabled(False)
+            #self.SelectFolderButton.setEnabled(False)
             
             self._file_to_save = name
             self._SelectFile.setText(self._file_to_save) 
